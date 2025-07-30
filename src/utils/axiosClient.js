1720f8c68a0e0775bd2e8dda02aa4fa3b1e3a816
shrijit-dhande7 <shrijitdhande@gmail.com>
2025-07-30 17:06:09 +0530

    Update axiosClient.js

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosClient =  axios.create({
-    baseURL: 'https://vercel.com/shrijit-dhandes-projects/code-decode-front-end-ztb6',
+    baseURL: 'https://code-decode-front-end-ztb6.vercel.app/',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
