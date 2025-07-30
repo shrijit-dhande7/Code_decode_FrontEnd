213f10706014daaf6135abf01693ea6cc2877cc1
Shrijit Dhande <shrijitdhande@gmail.com>
2025-07-30 11:19:52 +0530

    url bacend add

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosClient =  axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://code-decode-front-end-ztb6.vercel.app',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
